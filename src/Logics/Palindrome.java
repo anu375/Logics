fde8177391a019051582c6c34ca41342c34215ff
Anushka Jadhav <anujadhav32004@gmail.com>
2025-12-03 13:04:35 +0530

    first commit

---
+++ src/Logics/Palindrome.java
@@ -0,0 +1,24 @@
+package Logics;
+
+public class Palindrome
+{
+
+	public static void main(String[] args)
+	
+	{
+		
+		String str = "MADAM";
+		String reversed = new StringBuilder(str).reverse().toString();
+		//System.out.println(str.equals(reversed));
+		
+		if(str.equals(reversed))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
+
+}
